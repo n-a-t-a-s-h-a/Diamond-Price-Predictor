6dbb237bf7fa493b0563b17cb451cf7fb5941616
Samuel Githara <160905400+Samuel-Githara@users.noreply.github.com>
2025-07-10 11:11:03 +0300

    Update Untitled8.py

--- Untitled8.py
+++ Untitled8.py
@@ -6,7 +6,7 @@
 
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
